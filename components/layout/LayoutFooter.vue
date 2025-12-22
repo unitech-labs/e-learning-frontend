fbd53c207f83c3b2d00b826fc8eeda525152d413
huynamboz <teahuynam2003@gmail.com>
2025-12-22 16:55:04 +0700

    fix text

--- components/layout/LayoutFooter.vue
+++ components/layout/LayoutFooter.vue
@@ -56,7 +56,7 @@ const socialMedia: SocialMedia[] = [
 
 const companyInfo = {
   name: 'Phan Tâm',
-  description: 'Học tiếng Ý cùng cô Phan Tâm - giảng viên có nhiều năm kinh nghiệm, giúp bạn học ngôn ngữ một cách dễ dàng và thú vị.',
+  description: 'Học tiếng Ý cùng Phan Tâm - giảng viên có nhiều năm kinh nghiệm, giúp bạn học ngôn ngữ một cách dễ dàng và thú vị.',
 }
 </script>
 
