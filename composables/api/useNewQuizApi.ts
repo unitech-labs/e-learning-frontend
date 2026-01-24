f0785dc90e5ddd8c7955794cf99b96057bf671a2
huynamboz <teahuynam2003@gmail.com>
2026-01-24 14:55:23 +0700

    feat: fix quiz

--- composables/api/useNewQuizApi.ts
+++ composables/api/useNewQuizApi.ts
@@ -86,6 +86,7 @@ export interface NewQuiz {
   created_by_name: string
   created_at: string
   updated_at: string
+  user_attempts_count?: number // Number of attempts by current user (optional, returned by API)
 }
 
 export interface NewQuizDetail extends NewQuiz {
